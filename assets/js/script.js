b9d13dc138aba8a1a2489e186c5b28c9495ccdca
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-09-24 13:59:57 -0500

    setting up spotify API, one piece at a time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
+
+// Spotify SDK connection
 window.onSpotifyWebPlaybackSDKReady = () => {
-    const token = 'BQB-hlKahpdBd55qlh2we6bimzSTa299qoQPKp2YK0gUN7jlPuyFryXtDQA5zqTxzffgN_KyXbjFy4Mom7YbWfJpGtMKd16LfdW-LzWfNwmsQjzyeiXTGAyDcQvjnCcGMtXJwCnTMou7vUg36EPbdY46b8eEEedAsSODB--gW7goJHRyb3aaUrk';
+    const token = 'BQAKDRHeTKI8CGpTXel_W4PSM9hgh4I50GYseRU9BnAyWe6qrsydbhc5KOO4-odJNEXR0uIyZOGIvZNH57hn6xhHSFztSsYBLrqPeezMEob4PrnZbWbypPKALtudgRcQOM4eg-r6CPVmHCk5xN8VHRmqwTBoxS26_gremBxoGX3oWXKzm7tysBY';
     const player = new Spotify.Player({
       name: 'Web Playback SDK Quick Start Player',
       getOAuthToken: cb => { cb(token); },
@@ -33,6 +35,43 @@ window.onSpotifyWebPlaybackSDKReady = () => {
 
 };
 
+// TEST SPOTIFY CODE
+// Get the hash of the url
+const hash = window.location.hash
+.substring(1)
+.split('&')
+.reduce(function (initial, item) {
+  if (item) {
+    var parts = item.split('=');
+    initial[parts[0]] = decodeURIComponent(parts[1]);
+  }
+  return initial;
+}, {});
+window.location.hash = '';
+
+// Set token
+let _token = "BQAb80rmNMRiHEx1XQAh8SxlhOexRknlltu4Plbjh4z_vKIqb4_FxxK4n_HBtoM_MFOB28f79g3xlZLkni_S-XTVfB2nsQRPQUhRfjMpXUjo8IBATeANiqhpg627AJNO2b3Q-ISvIUEidNNBcnV7d5WeVk_IidD2UDQ";
+
+const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = '3157f22acedd463f8cf05d236076c33e';
+const redirectUri = 'https://james182-dang.github.io/Forecastify/';
+const scopes = [
+  'playlist-modify-public',
+  'playlist-read-public'
+];
+
+// If there is no token, redirect to Spotify authorization
+if (!_token) {
+  window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
+};
+
+
+// Functions to append the playlist iframe to the container div
+function getSunnyPlaylist() {
+    fetch("https://api.spotify.com/v1/playlists/37i9dQZF1DX4OzrY981I1W")
+}
 
 // Created a call to moment to get the date to put at the top of the page.
 var cday = document.querySelector("#currentDay");
