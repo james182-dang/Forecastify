e11e27104e35bb91aff546689ca634e935c000b5
Matt Roland <83994674+mattroland94@users.noreply.github.com>
2021-09-25 17:34:50 -0500

    Added the functionality of the weather app and added the city search with current weather and 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
+// DOM elements
+let elVerifyButton = document.getElementById("verifySpotify");
+
 window.onSpotifyWebPlaybackSDKReady = () => {
-    const token = 'BQB-hlKahpdBd55qlh2we6bimzSTa299qoQPKp2YK0gUN7jlPuyFryXtDQA5zqTxzffgN_KyXbjFy4Mom7YbWfJpGtMKd16LfdW-LzWfNwmsQjzyeiXTGAyDcQvjnCcGMtXJwCnTMou7vUg36EPbdY46b8eEEedAsSODB--gW7goJHRyb3aaUrk';
+    const token = 'BQAKDRHeTKI8CGpTXel_W4PSM9hgh4I50GYseRU9BnAyWe6qrsydbhc5KOO4-odJNEXR0uIyZOGIvZNH57hn6xhHSFztSsYBLrqPeezMEob4PrnZbWbypPKALtudgRcQOM4eg-r6CPVmHCk5xN8VHRmqwTBoxS26_gremBxoGX3oWXKzm7tysBY';
     const player = new Spotify.Player({
       name: 'Web Playback SDK Quick Start Player',
       getOAuthToken: cb => { cb(token); },
@@ -46,9 +49,72 @@ $(document).ready(function () {
   var lat = "";
   var lon = "";
 
+  // This is another call to the API to retrieve the rest of the current weather and daily weather
   function weatherGrabAPI(a,b) {
-    
-  }
+    weathURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + a + "&lon=" + b + "&exclude=minutely,hourly&appid=aec299195260a001b09706b5bfe740f7&units=imperial";
+
+    // The actual API key to get the rest of the current weathre and 5 day forecast
+    $.ajax({
+      url: queryURL2,
+      method: "GET"
+    }) .then(function(respons) {
+      console.log(response);
+
+      // This will remove the data that was in the 5-day forecast previously
+      $("#fiveday").empty();
+
+      // Grabs the weather icon and adds it to the page
+      var icon = response.current.weather[0].icon;
+      var iconImg = $("<img>");
+      icon.attr("src", "https://openweathermap.org/img/wn/" + icon + "@2x.png")
+      $("#city").append(iconImg);
+
+      // populates the IDs with the weather data
+      $("#temp").text("Temperature: " + response.current.temp + "° F");
+      $("#humidity").text("Humidity: " + response.current.humidity + "%");
+      $("#wind").text("Wind Speed: " + response.current.wind_speed + " MPH");
+      $("#uv-index").text(response.current.uvi);
+
+      // This dispalys the html to the user
+      $("currentweather").css({"display":"block"});
+
+      // array built to hold the daily response from the api
+      var fivedayf = response.daily;
+
+      // a for loop to grab the forecast for the next 5 days
+      for (i = 1; i < daily.length - 2; i++) {
+        var ddate = moment.unix(daily[i].dt).format("dddd MM/DD/YYYY");
+        var dtemp = daily[i].temp.day;
+        var dhum = daily[i].humidity;
+        var dicon = daily[i].weather[0].icon;
+
+        // creates the elements to hold the data
+        var ddiv = $("<div>");
+        var ptemp = $("<p>");
+        var phum = $("<p>");
+        var imgicon = $("<img>");
+        var hdate = $("<h6>");
+
+
+        // adds the text to the elements made above
+        hdate.text(ddate);
+        imgicon.attr("src", "https://openweathermap.org/img/wn/" + dicon + "@2x.png");
+        imgicon.css({"width": "100%"});
+        ptemp.text("Temp: " + dtemp + "° F");
+        phum.text("Humidity: " + dhum + "%");
+
+        // This appends the elements made above to the html
+        ddiv.append(hdate);
+        ddiv.append(imgicon);
+        ddiv.append(ptemp);
+        ddiv.append(phum);
+        $("#5fore").append(ddiv);
+
+        // This displays the html to the user
+        $("#fiveday").css({"display":"block"});
+      }
+    })
+  };
 
   // This function calls the weather API and inputs the users values
   function weatherGrab() {
@@ -68,7 +134,7 @@ $(document).ready(function () {
       localStorage.setItem("cityname", response.name);
 
       weatherGrabAPI();
-    })
+    });
   };
 
   function searchButton() {
@@ -84,11 +150,11 @@ $(document).ready(function () {
   $("#cityform").submit(function(event) {
     event.preventDefault();
     searchButton();
-  })
+  });
 
-  $("#citysubmit").submit(function(event) {
+  $("#citysubmit").click(function(event) {
     event.preventDefault();
     searchButton();
-  })
+  });
 
-})
+});
