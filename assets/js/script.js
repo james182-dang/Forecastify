a8a7c245592451da5c9fd9a75920461708aaf8c2
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-09-27 18:58:48 -0500

    pushing before class, further alternate verification testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,12 +37,52 @@ window.onSpotifyWebPlaybackSDKReady = () => {
 
 };
 
-// Code verifier and challenge functions
+// Code verifier and challenge functions TESTING
+
+// function dec2hex(dec) {
+//     return ('0' + dec.toString(16)).substr(-2)
+//   }
+  
+// function generateRandomString() {
+//     var array = new Uint32Array(56/2);
+//     window.crypto.getRandomValues(array);
+//     return Array.from(array, dec2hex).join('');
+// }
+  
+// var verifier = generateRandomString();
+
+// function sha256(plain) { // returns promise ArrayBuffer
+//     const encoder = new TextEncoder();
+//     const data = encoder.encode(plain);
+//     return window.crypto.subtle.digest('SHA-256', data);
+// }
+  
+// function base64urlencode(a) {
+//     var str = "";
+//     var bytes = new Uint8Array(a);
+//     var len = bytes.byteLength;
+//     for (var i = 0; i < len; i++) {
+//       str += String.fromCharCode(bytes[i]);
+//     }
+//     return btoa(str)
+//       .replace(/\+/g, "-")
+//       .replace(/\//g, "_")
+//       .replace(/=+$/, "");
+// }
+  
+// async function challenge_from_verifier(v) {
+//     hashed = await sha256(v);
+//     base64encoded = base64urlencode(hashed);
+//     return base64encoded;
+// }
+  
+// var challenge = await challenge_from_verifier(verifier);
+
 
 
 function verifySpotify() {
 
-   const result = fetch('https://accounts.spotify.com/authorize?client_id=3157f22acedd463f8cf05d236076c33e&response_type=code&redirect_uri=https://james182-dang.github.io/Forecastify/&code_challenge_method=S256&code_challenge=a63ILMPduQMeN_ZewWIlzN6VcOZeA9J1tAH7g_hNSwg', {
+    const result = fetch('https://accounts.spotify.com/authorize?client_id=3157f22acedd463f8cf05d236076c33e&response_type=code&redirect_uri=https://james182-dang.github.io/Forecastify/&code_challenge_method=S256&code_challenge=a63ILMPduQMeN_ZewWIlzN6VcOZeA9J1tAH7g_hNSwg', {
        method: ''
    })
     
@@ -53,7 +93,7 @@ function verifySpotify() {
 function APIController() {
 
     let clientID = "3157f22acedd463f8cf05d236076c33e";
-    let clientSecret = "1b6cfcf8571647edbb2667eefce03653"
+    let clientSecret = "1b6cfcf8571647edbb2667eefce03653";
         
         const result = fetch('https://accounts.spotify.com/api/token', {
             method: 'POST',
@@ -105,7 +145,7 @@ function getPlaylist() {
     // loginForm.innerHTML = 
 
     // dataContainer.appendChild(loginForm);
-};
+  //};
 
 elVerifyButton.addEventListener("click", verifySpotify);
 
