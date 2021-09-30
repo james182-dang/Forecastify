dea93f01f0e4ba5698b96cb82c4b8156d4494b88
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-09-29 19:54:12 -0500

    playlist/weather functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -223,8 +223,10 @@ $(document).ready(function () {
         // This displays the html to the user
         $("#fiveday").css(" box-border border-2");
       }
-    // Spotify authorization  
+    // Spotify playlist connections 
     }) .then (function(response) {
+
+                  
         // let clientID = "3157f22acedd463f8cf05d236076c33e";
         // let clientSecret = "1b6cfcf8571647edbb2667eefce03653";
             
@@ -237,24 +239,39 @@ $(document).ready(function () {
         //         body: 'grant_type=client_credentials'
         //     })
 
+        // Target, div creation for iframe
         let spotifyMainContainer = $("#spotify-main-container");
         let iFrameDiv = $("<div>");
 
+        // Clouds
         if (response.current.weather[0].main = "Clouds") {
             iFrameDiv;
             iFrameDiv.innerHTML = '<iframe src="https://open.spotify.com/embed/playlist/1Ers2ZxZT2WTcOwIxEWUnb" width="50%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
             spotifyMainContainer.append(iFrameDiv);
+
+        // Clear
         } else if (response.current.weather[0].main = "Clear") {
             iFrameDiv;
             iFrameDiv.innerHTML = '<iframe src="https://open.spotify.com/embed/playlist/1e82JSBwrnZF8TODtUcHeR" width="50%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
             spotifyMainContainer.append(iFrameDiv);
+
+        // Thunderstorm
         } else if (response.current.weather[0].main = "Thunderstorm") {
             iFrameDiv;
             iFrameDiv.innerHTML = '<iframe src="https://open.spotify.com/embed/playlist/43E16ip1D8xU9Ij8Fqj698" width="50%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
             spotifyMainContainer.append(iFrameDiv);
+        
+        // Drizzle
+        } else if (response.current.weather[0].main = "Drizzle") {
+            iFrameDiv;
+            iFrameDiv.innerHTML = ''
+            spotifyMainContainer.append(iFrameDiv);
+
+        // No weather at all?
         } else {
-            console.log("Weather has stopped existing.")
+            console.log("Weather has stopped existing. Please make sure you are still connected to the third dimension.")
         };
+
     })
   };
 
