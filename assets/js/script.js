0890a942f56009c76f2f8430e0aa7a213800bea3
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-10-02 12:39:42 -0500

    cleaned up comments in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,56 +48,7 @@ window.onSpotifyWebPlaybackSDKReady = () => {
 
 };
 
-// Code verifier and challenge functions TESTING
 
-// function dec2hex(dec) {
-//     return ('0' + dec.toString(16)).substr(-2)
-//   }
-  
-// function generateRandomString() {
-//     var array = new Uint32Array(56/2);
-//     window.crypto.getRandomValues(array);
-//     return Array.from(array, dec2hex).join('');
-// }
-  
-// var verifier = generateRandomString();
-
-// function sha256(plain) { // returns promise ArrayBuffer
-//     const encoder = new TextEncoder();
-//     const data = encoder.encode(plain);
-//     return window.crypto.subtle.digest('SHA-256', data);
-// }
-  
-// function base64urlencode(a) {
-//     var str = "";
-//     var bytes = new Uint8Array(a);
-//     var len = bytes.byteLength;
-//     for (var i = 0; i < len; i++) {
-//       str += String.fromCharCode(bytes[i]);
-//     }
-//     return btoa(str)
-//       .replace(/\+/g, "-")
-//       .replace(/\//g, "_")
-//       .replace(/=+$/, "");
-// }
-  
-// async function challenge_from_verifier(v) {
-//     hashed = await sha256(v);
-//     base64encoded = base64urlencode(hashed);
-//     return base64encoded;
-// }
-  
-// var challenge = await challenge_from_verifier(verifier);
-
-
-
-// function verifySpotify() {
-
-//     const result = fetch('https://accounts.spotify.com/authorize?client_id=3157f22acedd463f8cf05d236076c33e&response_type=code&redirect_uri=https://james182-dang.github.io/Forecastify/&code_challenge_method=S256&code_challenge=a63ILMPduQMeN_ZewWIlzN6VcOZeA9J1tAH7g_hNSwg', {
-//        method: ''
-//    })
-    
-// };
 
 // Function to authenticate Spotify on load, returns playlist data as check
 function APIController() {
@@ -257,53 +208,6 @@ $(document).ready(function () {
             console.log("Weather has stopped existing. Please make sure you are still connected to the third dimension.")
         };
       // }
-    // Spotify playlist connections 
-
-                  
-        // let clientID = "3157f22acedd463f8cf05d236076c33e";
-        // let clientSecret = "1b6cfcf8571647edbb2667eefce03653";
-            
-        //     const result = fetch('https://accounts.spotify.com/api/token', {
-        //         method: 'POST',
-        //         headers: {
-        //             'Content-Type' : 'application/x-www-form-urlencoded',
-        //             'Authorization' : 'Basic ' + btoa(clientID + ':' + clientSecret)
-        //         },
-        //         body: 'grant_type=client_credentials'
-        //     })
-
-        // Target, div creation for iframe
-        // let spotifyMainContainer = $("#spotify-main-container");
-        // let iFrameDiv = $("<div>");
-
-        // Clouds
-        // if (response.current.weather[0].main == "Clouds") {
-        //     iFrameDiv;
-        //     iFrameDiv.innerHTML = '<iframe src="https://open.spotify.com/embed/playlist/1Ers2ZxZT2WTcOwIxEWUnb" width="50%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
-        //     spotifyMainContainer.append(iFrameDiv);
-
-        // Clear
-        // } else if (response.current.weather[0].main = "Clear") {
-        //     iFrameDiv;
-        //     iFrameDiv.innerHTML = '<iframe src="https://open.spotify.com/embed/playlist/1e82JSBwrnZF8TODtUcHeR" width="50%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
-        //     spotifyMainContainer.append(iFrameDiv);
-
-        // Thunderstorm
-        // } else if (response.current.weather[0].main = "Thunderstorm") {
-        //     iFrameDiv;
-        //     iFrameDiv.innerHTML = '<iframe src="https://open.spotify.com/embed/playlist/43E16ip1D8xU9Ij8Fqj698" width="50%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
-        //     spotifyMainContainer.append(iFrameDiv);
-        
-        // Drizzle
-        // } else if (response.current.weather[0].main = "Drizzle") {
-        //     iFrameDiv;
-        //     iFrameDiv.innerHTML = ''
-        //     spotifyMainContainer.append(iFrameDiv);
-
-        // No weather at all?
-        // } else {
-        //     console.log("Weather has stopped existing. Please make sure you are still connected to the third dimension.")
-        // };
 
       // // array built to hold the daily response from the api
       // var fivedayf = response.daily;
